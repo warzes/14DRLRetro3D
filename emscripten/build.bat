58255a6adf9afdeb797553434faf7d7e4b485710
warzes <warzes@mail.ru>
2023-03-07 13:42:32 +1000

    - правки билда под веб

--- emscripten/build.bat
+++ emscripten/build.bat
@@ -1,4 +1,12 @@
-@call emcc -sUSE_GLFW=3 -sFULL_ES3=1 -sSTB_IMAGE=1 -sWASM=1 -std=c++2b -Os -Wall -fno-rtti -fno-exceptions -s -s FORCE_FILESYSTEM=1 -o ../bin/game.html --preload-file ../bin/data/@data/ -I. -I../src/3rdparty/ ../src/Game/Engine.cpp ../src/Game/GameApp.cpp ../src/Game/main.cpp
+@call emcc                                                                  ^
+	-sUSE_GLFW=3 -sFULL_ES3=1 -sSTB_IMAGE=1 -sWASM=1 -sFORCE_FILESYSTEM=1   ^
+	-std=c++2b -O3 -Wall -fno-rtti -fno-exceptions                          ^
+	-o ../bin/game.html                                                     ^
+	--preload-file ../bin/data/@data/                                       ^
+	-I../src/3rdparty/                                                      ^
+		../src/Game/Engine.cpp                                              ^
+		../src/Game/GameApp.cpp                                             ^
+		../src/Game/main.cpp
 
 erase "../bin/index.html"
 ren "../bin/game.html" "../bin/index.html"
